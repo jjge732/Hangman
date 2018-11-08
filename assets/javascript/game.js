d47a2ff140e8fe5627a723a056e53fc0a435af21
jeast <eastwoodj000@gmail.com>
2018-11-08 10:20:50 -0600

    added styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,80 @@
-let words = 2;
-const methods = 3;
+let words = 0;
+const variables = 3;
+const methods = 4;
 
 let game = [
     {
-        blank: ['_', ' ', '_', ' ', '_', , '_', ,'_'],
-        word: 'h e l l o'
+        blank: ['_', '_', '_', '_'],
+        word: ['h', 'i', 'y', 'a']
     },
     {
-        blank: '_ _ _ _ _ _ _',
-        word: 'w e l c o m e'
+        blank: ['_', '_', '_', '_', '_', '_', '_'],
+        word: ['w', 'h', 'a', 't', 's', 'u', 'p']
     },
     {
         blank: ['_', '_', '_', '_'],
-        word: 'hola'
+        word: ['h', 'o', 'l', 'a']
+    },
+    {   
+        usedLetters: [],
+        newWord: document.getElementById('blanks'),
+        guesses: document.getElementById('past_guesses')
     },
     {
-         get() {
+        gameOver() {
+            alert('Game over!')
+            alert(`The last word was ${game[words].word.toString()}`);
+            words++;
+            game[variables].usedLetters = [];
+            game[variables].guesses.innerHTML = game[variables].usedLetters.toString();
+            if (words < 3) {
+                alert('Next Word!')
+            }
+            else {
+                alert('No more words');
+            }
+            game[variables].newWord.innerHTML = game[words].blank.toString();
+        },
 
+        youWin() {
+            alert('Winner!')
+            words++;
+            game[variables].usedLetters = [];
+            game[variables].guesses.innerHTML = game[variables].usedLetters.toString();
+            if (words < 3) {
+                alert('Next Word!')
+            }
+            else {
+                alert('No more words');
+            }
+            game[variables].newWord.innerHTML = game[words].blank.toString();
         }
     }
 ];
 
-let usedLetters = [];
-let newWord = document.getElementById('blanks');
-let guesses = document.getElementById('past_guesses');
+// let usedLetters = [];
+// let newWord = document.getElementById('blanks');
+// let guesses = document.getElementById('past_guesses');
+game[variables].newWord.innerHTML = game[words].blank.toString();
 
 document.onkeyup = function(event) {
     if (typeof event.key === 'string' && event.key.length === 1) {
-        usedLetters.push(event.key);
-        if (game[words].word.indexOf(event.key) !== -1) {
+       
+        if (game[variables].usedLetters.indexOf(event.key) !== -1) {
+            alert(`${event.key} already guessed.`);
+        }
+        else if (game[words].word.indexOf(event.key) !== -1) {
             game[words].blank[game[words].word.indexOf(event.key)] = event.key;
-            console.log(game[words].blank.toString());
+            game[variables].newWord.innerHTML = game[words].blank.toString();
+            if (game[words].blank.toString() === game[words].word.toString()) {
+                game[methods].youWin();
+            }
         }
         else {
-            guesses.innerHTML = usedLetters;
-            for (let i = 0; i < usedLetters.length; i++) {
-                guesses = usedLetters[i];
+            game[variables].usedLetters.push(event.key);
+            game[variables].guesses.innerHTML = game[variables].usedLetters.toString();
+            if (game[variables].usedLetters.length > 7) {
+                game[methods].gameOver();
             }
         }
         
